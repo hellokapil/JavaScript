69bbf1e6af5803a6e5949d030513a50327733a8a
hellokapil <hellokapil@outlook.com>
2023-06-21 15:27:02 +0530

    Commented

---
+++ function2.js
@@ -0,0 +1,10 @@
+function Sum(a,b) { 
+    console.log(a + b);
+    return a + b;
+
+}
+const a = Sum(10, 50);
+console.log(a);
+
+/* Parameters are like variable which is specific to that function.
+   Parameter defined when we call the function and pass arguments */
