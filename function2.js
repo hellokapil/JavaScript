f75d16c8598c251388693175c8820d086f36380f
hellokapil <hellokapil@outlook.com>
2023-06-21 22:55:11 +0530

    Function2.js Changes

--- function2.js
+++ function2.js
@@ -1,10 +1,27 @@
-function Sum(a,b) { 
-    console.log(a + b);
-    return a + b;
+function Sum(a,b){          // Tradational Way to declare a function
+  
+  return a+b;
+
 
 }
-const a = Sum(10, 50);
-console.log(a);
+
+
+
+const = Sum function (a,b){         // Function Expression And also Called Anonymous Function
+  return a+b;
+}
+
+
+let a = Sum(50,50)
+console.log(a)
+
+
+const Sum=(a,b) => {        // Arrow Function 
+  return a+b;
+}
+console.log(Sum(10,29));
+
 
 /* Parameters are like variable which is specific to that function.
+
    Parameter defined when we call the function and pass arguments */
